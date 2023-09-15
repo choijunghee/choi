5acc2bd7f2681dd52e6d7471b990bb520f7db109
choijunghee <bcfkorea@hanmail.net>
2023-09-15 21:00:36 +0900

    js-menu

--- src/main.js
+++ src/main.js
@@ -24,3 +24,15 @@ document.addEventListener('scroll', ()=>{
     arrowUp.style.opacity=0;
  }
 });
+
+//Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header_menu');
+const  NavbarToggle = document. querySelector('.header_toggle');
+NavbarToggle.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+});
+
+//Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', ()=>{
+    navbarMenu.classList.remove('open');
+});
