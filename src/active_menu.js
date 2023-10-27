207c32d2e9c590c0eb97cd7a0bab42dcf24a8195
choijunghee <bcfkorea@hanmail.net>
2023-10-27 09:35:34 +0900

    sectionid
    section

--- src/active_menu.js
+++ src/active_menu.js
@@ -6,4 +6,23 @@
 //다수의세션이 동시에 보여진다면 가장 첫번째 섹션을 선택
 //마지막 contact 섹션이 보여진다면 그러면 가장 마지막 섹션을 선택
 const sectionIds = ['#home','#about','#skills','#work','#recommend' ,'#contact'];
-const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`));
+const visibleSections= sectionIds.map(()=> false);
+
+const Options = {};
+const observer = new IntersectionObserver( observerCallback, Options);
+Selections.forEach(section=>boserver.boserve(section));
+
+function observerCallback (entries){
+    let selectLastOne;
+    entries.forEach((entry)=>{
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.intersectiing;
+        selectLastOne = index === sectionIds. length -1 && entry. isIntersectiing && entry. IntersectionRatio>= 0.99;
+    });
+    const navItems = selectLastOne ? sectionIds. length-1 : findFirstIntersecting(visibleSections);
+    function findFirstIntersecting(intersections){
+        const index = intersections. indexOf(true);
+        return index => 0 ? index : 0 
+    }
+}
